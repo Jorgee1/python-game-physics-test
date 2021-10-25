4a1bcc0bb49ea7c5d72e80b671ede3b074e70ed1
Jorgee1 <jorge.ortega.p.1@gmail.com>
2021-10-24 20:24:08 -0500

    Fixed bug on top_down that would allow entities to clip throught walls on corners and get stuck

--- top_down.py
+++ top_down.py
@@ -3,6 +3,11 @@ import random as r
 import time as t
 import pygame as pg
 
+class Bool2:
+    def __init__(self, x=False, y=False):
+        self.x = x
+        self.y = y
+
 class Color:
     black = ( 25, 25, 25)
     white = (200,200,200)
@@ -33,7 +38,7 @@ class Entity:
         self.collider = box
         self.speed = pg.Vector2()
         self.color = color
-
+        self.collision = Bool2()
     def update(self):
         self.collider.x += self.speed.x
         self.collider.y += self.speed.y
@@ -62,8 +67,9 @@ def check_collition(A: Box, B: Box):
         (A_DER >= B_IZQ) and (A_IZQ <= B_DER)
     )
 
+n = 20
 fps = 60
-step = 10
+step = 3
 game_exit = False
 
 screen = Box(0,0,640,480)
@@ -95,7 +101,7 @@ static_list = [wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9]
 render_list = [p1, wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9]
 
 
-for i in range(20):
+for i in range(n):
     dummy = Entity(Box(20+i*55,330,50,50), Color.blue)
 
     dummy.speed.x = (1+r.random())*2*(-1)**i
@@ -135,28 +141,43 @@ while not game_exit:
 
     # Colition
     for box in dynamic_list:
+        box.collision.x = False
+        box.collision.y = False
         for wall in static_list:
             for i in range(1, step+1):
                 f_box = box.collider.copy()
                 f_box.x += i*box.speed.x/step
                 f_box.y += i*box.speed.y/step
 
-                if check_collition(f_box, wall.collider):
-                    f_box_x = box.collider.copy()
-                    f_box_x.x += i*box.speed.x/step
-                    f_box_x.y += (i-1)*box.speed.x/step
+                f_box_x = box.collider.copy()
+                f_box_x.x += i*box.speed.x/step
+                f_box_x.y += (i-1)*box.speed.y/step
 
-                    f_box_y = box.collider.copy()
-                    f_box_y.x += (i-1)*box.speed.x/step
-                    f_box_y.y += i*box.speed.y/step
+                f_box_y = box.collider.copy()
+                f_box_y.x += (i-1)*box.speed.x/step
+                f_box_y.y += i*box.speed.y/step
 
-                    if check_collition(f_box_x, wall.collider):
-                        box.speed.x = (i-1)*box.speed.x/step
+                colition_x = check_collition(f_box_x, wall.collider)
+                colition_y = check_collition(f_box_y, wall.collider)
+                colition_xy = check_collition(f_box, wall.collider)
 
-                    if check_collition(f_box_y, wall.collider): 
-                        box.speed.y = (i-1)*box.speed.y/step
-
+                if colition_x and not colition_y:
+                    box.collision.x = True
+                    box.speed.x = (i-1)*box.speed.x/step
                     break
+
+                if colition_y and not colition_x:
+                    box.collision.y = True
+                    box.speed.y = (i-1)*box.speed.y/step
+                    break
+
+                if colition_xy:
+                    box.collision.x = True
+                    box.collision.y = True
+                    box.speed.x = (i-1)*box.speed.x/step
+                    box.speed.y = (i-1)*box.speed.y/step
+                    break
+
 
     # Update
     for box in dynamic_list:
