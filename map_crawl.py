cabf475f680a042839123cfea9783554909378fa
Jorgee1 <jorge.ortega.p.1@gmail.com>
2021-10-30 21:21:49 -0500

    Added transitions.py, show a screen animation when warping. map_crawl.py is an aplication of the warp where 3 roms are connected

--- map_crawl.py
+++ map_crawl.py
@@ -1,14 +1,13 @@
-import math
 import time as t
 import pygame as pg
 
 class Color:
-    black = ( 25, 25, 25)
-    gray  = ( 30, 30, 30)
+    black = (25,25,25)
+    grey  = (40,40,40)
     white = (200,200,200)
-    red   = (200, 25, 25)
-    green = ( 25,200, 25)
-    blue  = ( 25,100,200)
+    red   = (200,100,25)
+    green = (25,200,25)
+    blue  = (25,100,200)
 
 class Bool2:
     def __init__(self, x=False, y=False):
@@ -24,7 +23,7 @@ class Box:
 
     def copy(self):
         return Box(self.x, self.y, self.w, self.h)
-
+    
     @property
     def rect(self):
         return pg.Rect(self.x, self.y, self.w, self.h)
@@ -33,9 +32,10 @@ class Box:
     def size(self):
         return (self.w, self.h)
 
-class Event:
-    def __init__(self, id, collider, color):
+class Warp:
+    def __init__(self, id, collider: Box, color):
         self.id = id
+        self.spawn = pg.Vector2()
         self.collider = collider
         self.color = color
 
@@ -43,25 +43,18 @@ class Entity:
     def __init__(self, collider, max_speed, color):
         self.collider = collider
         self.speed = pg.Vector2()
-        self.collision = Bool2()
-        self.color = color
         self.max_speed = max_speed
-    
+        self.color = color
+        self.collision = Bool2()
+
     def update(self):
         self.collider.x += self.speed.x
         self.collider.y += self.speed.y
 
+
 def draw_box(box: Box, color: Color):
     surface = pg.display.get_surface()
     pg.draw.rect(surface, color, box.rect, width=1)
-
-def create_box(x, y, wall_w, wall_h):
-    return [
-        Entity(Box(x,y,wall_h,wall_w), 0, Color.green),
-        Entity(Box(x,y,wall_w,wall_h), 0, Color.green),
-        Entity(Box(x+wall_h-wall_w,y,wall_w,wall_h), 0, Color.green),
-        Entity(Box(x,y+wall_h-wall_w,wall_h,wall_w), 0, Color.green)
-    ]
 
 def check_collition(A: Box, B: Box):
     # A Edges
@@ -83,74 +76,100 @@ def check_collition(A: Box, B: Box):
         (A_DER >= B_IZQ) and (A_IZQ <= B_DER)
     )
 
+def create_box(x, y, wall_w, wall_h):
+    return [
+        Entity(Box(x,y,wall_w,wall_h), 0, Color.green),
+        Entity(Box(x,y,wall_h,wall_w), 0, Color.green),
+        Entity(Box(x,y+wall_w-wall_h,wall_w,wall_h), 0, Color.green),
+        Entity(Box(x+wall_w-wall_h,y,wall_h,wall_w), 0, Color.green)
+    ]
+
 fps = 60
 step = 10
-wall_w = 20
 game_exit = False
-
-
+transition = True
 screen = Box(0,0,640,480)
 
-map_id=1
-# Map 1
-box1 = create_box(10,50,20,400)
-box1_start = pg.Vector2(160, 160)
 
-# Map 2
-box2 = create_box(100,100,20,300)
-box2_start = pg.Vector2(200, 160)
-wall1 = Entity(Box(249,300,40,40), 0, Color.green)
+p1 = Entity(Box(0,0,40,40), 5, Color.red)
 
-p1 = Entity(Box(box1_start.x,box1_start.y,40,40), 10, Color.red)
-
-event1 = Event(1,Box(40,80,20,20), Color.blue)
-event2 = Event(2,Box(150,250,20,20), Color.blue)
-
-dynamic_list = []
 event_list = []
+dynamic_list = []
 static_list = []
 render_list = []
 
-transition = True
-transition_start = False
-transition_count = 0
+map_id = 1
+# MAP 1
+map1 = create_box(640/2-150,480/2-150,300,10)
+start1 = pg.Vector2(640/2-20,480/2+50)
+event1 = Warp(1,Box(640/2-10,480/2-150+20,20,20), Color.blue)
+event1.spawn.x = 640/2 - p1.collider.w/2
+event1.spawn.y = event1.collider.y + event1.collider.h + 10
+
+# Map2
+start2 = pg.Vector2(640/2,480/2)
+map2 = create_box(640/2-200,480/2-200,400,10)
+event2 = Warp(2,Box(640/2-10,480/2-200+20,20,20), Color.blue)
+event2.spawn.x = 640/2 - p1.collider.w/2
+event2.spawn.y = event2.collider.y + event2.collider.h + 10
+event4 = Warp(4,Box(640/2-10,480/2+200-40,20,20), Color.blue)
+event4.spawn.x = 640/2 - p1.collider.w/2
+event4.spawn.y = event4.collider.y - event4.collider.h - p1.collider.h
+
+# Map3
+start3 = pg.Vector2(200,200)
+map3 = create_box(640/2-150,480/2-150,300,10)
+event3 = Warp(3,Box(640/2-10,480/2+ 150 - 40,20,20), Color.blue)
+event3.spawn.x = 640/2 - p1.collider.w/2
+event3.spawn.y = event3.collider.y - event3.collider.h - p1.collider.h
+block = Entity(Box(640/2-90,480/2-90,40,40),0, Color.green)
 
 pg.init()
 pg.display.set_mode(screen.size)
 
 while not game_exit:
     ref_time = t.time()
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game_exit = True
-            break
 
-    # Transition
+    # Map Transition
     if transition:
         if map_id == 1:
-            p1.collider.x = box1_start.x
-            p1.collider.y = box1_start.y
-            p1.speed.x = 0
-            p1.speed.y = 0
+            p1.collider.x = start1.x
+            p1.collider.y = start1.y
+            p1.speed = pg.Vector2()
 
-            dynamic_list = [p1]
             event_list = [event1]
-            static_list = [] + box1
-            render_list = [p1, event1] + box1
-
-        elif map_id == 2:
-            p1.collider.x = box2_start.x
-            p1.collider.y = box2_start.y
-            p1.speed.x = 0
-            p1.speed.y = 0
-
             dynamic_list = [p1]
-            event_list = [event2]
-            static_list = [wall1] + box2
-            render_list = [p1, event2, wall1] + box2
+            static_list = map1
+            render_list = dynamic_list + static_list + event_list
+            transition = False
+        elif map_id == 2:
+            p1.collider.x = start2.x
+            p1.collider.y = start2.y
+            p1.speed = pg.Vector2()
 
-        transition = False
+            event_list = [event2, event4]
+            dynamic_list = [p1]
+            static_list = map2
+            render_list = dynamic_list + static_list + event_list
+            transition = False
+        elif map_id == 3:
+            p1.collider.x = start3.x
+            p1.collider.y = start3.y
+            p1.speed = pg.Vector2()
+
+            event_list = [event3]
+            dynamic_list = [p1]
+            static_list = map3 + [block]
+            render_list = dynamic_list + static_list + event_list
+            transition = False
+        else:
+            map_id = 1
+
+
+
     # Input
     keys = pg.key.get_pressed()
 
@@ -168,15 +187,25 @@ while not game_exit:
     else:
         p1.speed.x = 0
 
-
     # Events
     for event in event_list:
         if check_collition(p1.collider, event.collider):
+
             if event.id == 1 and keys[pg.K_z]:
-                transition_start = True
+                transition = True
                 map_id = 2
-            if event.id == 2 and keys[pg.K_z]:
-                transition_start = True
+                start1 = event1.spawn
+                start2 = event4.spawn
+            elif event.id == 2 and keys[pg.K_z]:
+                transition = True
+                map_id = 3
+                start2 = event2.spawn
+                start3 = event3.spawn
+            elif event.id == 3 and keys[pg.K_z]:
+                transition = True
+                map_id = 2
+            elif event.id == 4 and keys[pg.K_z]:
+                transition = True
                 map_id = 1
 
     # Collition
@@ -237,9 +266,8 @@ while not game_exit:
         box.speed.y = speed_y
 
     # Update
-    if not transition_start:
-        for box in dynamic_list:
-            box.update()
+    for box in dynamic_list:
+        box.update()
 
     # Render
     surface = pg.display.get_surface()
@@ -248,26 +276,12 @@ while not game_exit:
     for box in render_list:
         draw_box(box.collider, box.color)
 
-    if transition_start:
-        rect = pg.Rect(0,0,screen.w, transition_count * screen.h/30)
-        pg.draw.rect(surface, Color.gray, rect)
-
     pg.display.flip()
 
-    # FPS Control
     timestamp = t.time() - ref_time
     if timestamp < 1/(fps+0.5):
         t.sleep(1/(fps+0.5) - timestamp)
-    
-    if transition_start:
-        if transition_count < 30:
-            transition_count += 1
-        elif transition_count >= 30:
-            transition_count = 0
-            transition_start = False
-            transition = True
-    
-    print(round(1/(t.time() - ref_time)), transition_count)
+
+    print(round(1/(t.time() - ref_time)))
 
 pg.quit()
-
